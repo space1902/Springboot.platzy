6f5231fd6c20398cf0372a8db6db4da67d6ed200
space1902 <victorup1902@gmail.com>
2022-05-14 14:46:32 -0500

    perimer commit

---
+++ src/main/java/com/fudamentosplatzi/springboot/fudamentos/FudamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fudamentosplatzi.springboot.fudamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FudamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FudamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fudamentosplatzi/springboot/fudamentos/FudamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fudamentosplatzi.springboot.fudamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FudamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
